5633e64dcdb6e2b1ca2e7c262851e758f9718265
Yanna-Trofimova <111576903+Yanna-Trofimova@users.noreply.github.com>
2023-08-11 20:22:14 +0300

    add components and styles

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import RegistrationScreen from './Screens/RegistrationScreen';
+import LoginScreen from './Screens/LoginScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <LoginScreen/>
+    <RegistrationScreen/>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
---
+++ Screens/LoginScreen.js
@@ -0,0 +1,111 @@
+import { useState } from 'react';
+import { StyleSheet, Text, View, ImageBackground, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform, Keyboard, TouchableWithoutFeedback,Image } from 'react-native';
+import { useFonts } from 'expo-font';
+
+
+
+export default function LoginScreen() {
+  const [isReady, setIsReady] = useState(false);
+
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("../assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("../assets/fonts/Roboto-Medium.ttf")
+  });
+
+    if (!fontsLoaded) {
+    return null;
+  }
+
+  const keyboardHide = () => {
+    Keyboard.dismiss()
+  }
+
+ 
+
+
+  return (
+    <View style={styles.container}>
+    
+    <TouchableWithoutFeedback onPress={()=> Keyboard.dismiss()}>
+      <ImageBackground
+        style={styles.image}
+              source={require('../assets/images/BG.jpg')} >
+          <KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"}>
+            <View style={styles.whiteContainer}>
+
+             <Text style={{ fontFamily: 'Roboto-Medium', fontSize: 30 ,color: "#212121", fontWeight: 700, marginHorizontal: 107, marginBottom: 32}}>Увійти</Text>
+              <View style={styles.form}>
+                  <TextInput style={styles.input }  placeholder="Адреса електронної пошти" />
+                  <TextInput style={styles.input}  secureTextEntry={true} placeholder="Пароль"/>
+                <TouchableOpacity activeOpacity={0.8} style={styles.btn} onPress={keyboardHide}>
+                  <Text style={
+                  { fontFamily: 'Roboto-Regular', fontSize: 16 ,color: "#ffff"}
+                }>Увійти</Text>
+              </TouchableOpacity>
+
+               <TouchableOpacity activeOpacity={0.8} style={styles.link}>
+                <Text style={{ fontFamily: 'Roboto-Regular', fontSize: 16 ,color: "#1B4371"  }}>Немає акаунту? <Text style={{ textDecorationLine:'underline' }}> Зареєструватися </Text> </Text>
+              </TouchableOpacity>
+              
+              </View>
+            </View>
+            </KeyboardAvoidingView>    
+        </ImageBackground>
+     </TouchableWithoutFeedback>     
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  whiteContainer: {
+    paddingTop: 32,
+    paddingBottom: 111,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: "flex-end",
+  },
+  input: {
+      borderWidth: 1,
+      borderColor: '#BDBDBD',
+      marginHorizontal: 16,
+      borderRadius: 10,
+      height: 50,
+      color: '#E8E8E8',
+      width: 343,
+      marginTop: 16,
+      backgroundColor: "#F6F6F6",
+      fontSize: 16,
+      lineHeight: 19,
+      paddingLeft: 16
+    },
+    btn: {
+        backgroundColor: "#FF6C00",
+        width: 343,
+        height: 51,
+        marginTop: 43,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginHorizontal: 16,
+        borderRadius: 100,
+  },
+    
+     link: {
+       marginTop: 16,
+       marginHorizontal: 61,
+        
+    },
+ 
+});
+
+
